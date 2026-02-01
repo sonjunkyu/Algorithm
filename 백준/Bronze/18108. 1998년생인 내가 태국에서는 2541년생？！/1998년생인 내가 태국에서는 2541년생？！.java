210eebac9161937e4fbbf6edb80d0ec9018949bd
손준규 <144649271+sonjunkyu@users.noreply.github.com>
2026-02-02 01:31:27 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 168 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int bulgiYear = sc.nextInt();
+
+        System.out.println(bulgiYear - 543);
+    }
+}
