382970699bc5f6d86497af2696e488a50d76d77c
손준규 <144649271+sonjunkyu@users.noreply.github.com>
2026-02-04 18:59:11 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 100 ms, Memory: 14344 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N - i; j++) {
+                sb.append(" ");
+            }
+
+            for (int k = 1; k <= i; k++) {
+                sb.append("*");
+            }
+
+            sb.append("\n");
+        }
+
+        System.out.print(sb);
+    }
+}
