34baed1be21092b8bce56c418e6e72e70a898f22
손준규 <144649271+sonjunkyu@users.noreply.github.com>
2026-02-24 19:56:58 +0900

    [Bronze II] Title: 소수, Time: 116 ms, Memory: 14208 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.java
@@ -0,0 +1,43 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        boolean isFound = false;
+
+        for (int i=M; i<=N; i++) {
+            if (isPrime(i)) {
+                sum += i;
+                if (!isFound) {
+                    min = i;
+                    isFound = true;
+                }
+            }
+        }
+
+        if (!isFound) {
+            System.out.println(-1);
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
+
+    private static boolean isPrime(int num) {
+        if (num < 2) return false;
+
+        for (int i=2; i<num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
