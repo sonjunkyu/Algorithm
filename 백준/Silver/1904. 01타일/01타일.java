06a5b41d1d103a8b7656f874117d5c72e006dc31
손준규 <144649271+sonjunkyu@users.noreply.github.com>
2026-04-03 02:08:54 +0900

    [Silver III] Title: 01타일, Time: 280 ms, Memory: 58048 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.java
+++ 백준/Silver/1904. 01타일/01타일.java
@@ -15,19 +15,14 @@ public class Main {
     }
 
     private static int fibonacci(int n) {
-        if (n == 1) {
-            return 1;
-        }
-        else if (n == 2) {
-            return 2;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        if (dp[n] != 0) {
+            return dp[n];
         }
 
-        dp[1] = 1;
-        dp[2] = 2;
-
-        for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i-1] + dp[i-2]) % 15746;
-        }
+        dp[n] = (fibonacci(n-1) + fibonacci(n-2)) % 15746;
 
         return dp[n];
     }
